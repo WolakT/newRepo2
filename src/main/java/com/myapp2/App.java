ea7a903072846e9f015add592ef8f82416ce03e3
WolakT <wolak.tom@gmail.com>
2017-04-21 21:11:09 +0200

    changes to code

--- src/main/java/com/myapp2/App.java
+++ src/main/java/com/myapp2/App.java
@@ -1,13 +1,20 @@
 package com.myapp2;
 
+import java.util.Currency;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        System.out.println("Hello World!!!!");
+        double bubu = 2.2;
+    }
+        public static void location (double v, double t, double ix){
+            double a = 9.81;
+        System.out.println(0.5*a*t+v*t +ix);
+
     }
 }
